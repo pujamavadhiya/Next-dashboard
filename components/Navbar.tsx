a8b64f76e12c43a668406b6abd372e4611487c97
pujamavadhiya <pujamavadhiya12@gmail.com>
2024-06-07 16:26:24 +0530

    added theme

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '../img/logo.jpg';
-import { Avatar,AvatarImage,AvatarFallback } from '@/components/ui/avatar';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,19 +9,22 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from '@/components/ui/dropdown-menu';
+import ThemeToggler from '@/components/ThemeToggler';
 
 const Navbar = () => {
   return (
-    <div className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between">
-        <Link href='/'>
-            <Image src={logo} alt='Traversy Media' width={40} />
-        </Link>
+    <div className='bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between'>
+      <Link href='/'>
+        <Image src={logo} alt='TraversyPress' width={40} />
+      </Link>
 
+      <div className='flex items-center'>
+        <ThemeToggler />
         <DropdownMenu>
           <DropdownMenuTrigger className='focus:outline-none'>
             <Avatar>
-              <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn'/>
+              <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
               <AvatarFallback className='text-black'>BT</AvatarFallback>
             </Avatar>
           </DropdownMenuTrigger>
@@ -36,9 +39,9 @@ const Navbar = () => {
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
