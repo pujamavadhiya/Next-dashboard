c5da2458d74ae5ac365e9d0e70126b8320b1b230
pujamavadhiya <pujamavadhiya12@gmail.com>
2024-06-07 16:12:46 +0530

    added theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar";
-import { Toaster } from "@/components/ui/toaster";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { Toaster } from '@/components/ui/toaster';
+import { ThemeProvider } from '@/components/providers/ThemeProvider';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "TraversyPress",
-  description: "Admin Dashboard",
+  title: 'TraversyPress',
+  description: 'Admin dashboard',
 };
 
 export default function RootLayout({
@@ -18,16 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <Navbar />
-        <div className="flex">
-          <div className="hidden md:block h-[100vh] w-[300px]">
-            <Sidebar />
-          </div>
-          <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
-        </div>
-        <Toaster />
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='light'
+          enableSystem={true}
+          storageKey='dashboard-theme'
+        >
+          {children}
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
